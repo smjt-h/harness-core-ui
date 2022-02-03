c90bde8e755dd47ab58906aee638007d163baf2d
deepesh-ui <deepesh.kumar@harness.io>
2022-02-03 11:38:56 +0530

    feat: [CVS-9002]: automation for AppDynamics and bugfixes

--- cypress/integration/85-cv/HealthSource/AppDHealthSource.spec.ts
+++ cypress/integration/85-cv/HealthSource/AppDHealthSource.spec.ts
@@ -42,6 +42,68 @@ describe('Create empty monitored service', () => {
     // Fill Customise HealthSource Tab for AppDynamics
     cy.wait('@ApplicationCall')
     cy.wait('@MetricPackCall')
+    cy.wait(1000)
+
+    cy.get('input[name="metricData.Errors"]').should('be.checked')
+    cy.get('input[name="metricData.Performance"]').should('be.checked')
+
+    // Validation
+    cy.contains('span', 'Submit').click({ force: true })
+
+    cy.contains('span', 'Please select applications').should('be.visible')
+
+    cy.get('input[name="metricData.Errors"]').uncheck({ force: true })
+    cy.get('input[name="metricData.Performance"]').uncheck({ force: true })
+    cy.contains('span', 'Submit').click({ force: true })
+    cy.contains('span', 'Plese select metric packs').should('be.visible')
+    cy.get('input[name="metricData.Errors"]').check({ force: true })
+    cy.get('input[name="metricData.Performance"]').check({ force: true })
+    cy.contains('span', 'Plese select metric packs').should('not.exist')
+
+    cy.get('input[name="appdApplication"]').click()
+    cy.contains('p', 'cv-app').click({ force: true })
+
+    // Validation
+    cy.contains('span', 'Please select applications').should('not.exist')
+    cy.contains('span', 'Submit').click({ force: true })
+    cy.contains('span', 'Please select tier').should('be.visible')
+
+    cy.wait('@TierCall')
+    cy.get('input[name="appDTier"]').click()
+    cy.contains('p', 'docker-tier').click({ force: true })
+
+    cy.contains('span', 'Submit').click({ force: true })
+
+    cy.contains('div', 'AppD').click({ force: true })
+    cy.contains('span', 'Next').click()
+
+    cy.get('input[name="appDTier"]').should('have.value', 'docker-tier')
+    cy.get('input[name="appdApplication"]').should('have.value', 'cv-app')
+    cy.contains('span', 'Submit').click({ force: true })
+  })
+
+  it('Add new monitored service with custom metric', () => {
+    cy.intercept('GET', applicationCall, applicationsResponse).as('ApplicationCall')
+    cy.intercept('GET', metricPackCall, metricPackResponse).as('MetricPackCall')
+    cy.intercept('GET', tiersCall, tiersResponse).as('TierCall')
+    cy.intercept('GET', basePathCall, basePathResponse).as('basePathCall')
+    cy.intercept('GET', metricStructureCall, metricStructureResponse).as('metricStructureCall')
+
+    cy.addNewMonitoredServiceWithServiceAndEnv()
+
+    cy.contains('span', 'Add New Health Source').click()
+
+    // Fill Define HealthSource Tab with AppDynamics
+    cy.get('span[data-icon="service-appdynamics"]').click()
+    cy.get('input[name="healthSourceName"]').type('AppD')
+    cy.get('button[data-testid="cr-field-connectorRef"]').click()
+    cy.contains('p', 'appdtest').click()
+    cy.contains('span', 'Apply Selected').click()
+    cy.contains('span', 'Next').click()
+
+    // Fill Customise HealthSource Tab for AppDynamics
+    cy.wait('@ApplicationCall')
+    cy.wait('@MetricPackCall')
 
     cy.get('input[name="appdApplication"]').click()
     cy.contains('p', 'cv-app').click({ force: true })
@@ -50,8 +112,14 @@ describe('Create empty monitored service', () => {
     cy.get('input[name="appDTier"]').click()
     cy.contains('p', 'docker-tier').click({ force: true })
 
-    // Custom metric
+    // Adding custom metric should make metric pack oprional
+    cy.get('input[name="metricData.Errors"]').uncheck({ force: true })
+    cy.get('input[name="metricData.Performance"]').uncheck({ force: true })
+    cy.contains('span', 'Submit').click({ force: true })
+    cy.contains('span', 'Plese select metric packs').should('be.visible')
     cy.contains('span', 'Add Metric').click()
+    cy.contains('span', 'Plese select metric packs').should('not.exist')
+
     cy.contains('div', 'Assign').click({ force: true })
 
     // Custom validation
@@ -77,5 +145,16 @@ describe('Create empty monitored service', () => {
 
     cy.contains('div', 'AppD').click({ force: true })
     cy.contains('span', 'Next').click()
+
+    cy.get('input[name="appDTier"]').should('have.value', 'docker-tier')
+    cy.get('input[name="appdApplication"]').should('have.value', 'cv-app')
+    cy.get('input[name="groupName"]').should('have.value', 'group 1')
+    cy.get('input[name="metricName"]').should('have.value', 'appdMetric')
+    cy.get('input[name="basePath"]').should('have.value', 'Overall Application Performance')
+    cy.get('input[name="metricPathDropdown"]').should('have.value', 'Calls per Minute')
+    cy.contains('p', 'Overall Application Performance / docker-tier / Calls per Minute')
+      .scrollIntoView()
+      .should('be.visible')
+    cy.contains('span', 'Submit').click({ force: true })
   })
 })
