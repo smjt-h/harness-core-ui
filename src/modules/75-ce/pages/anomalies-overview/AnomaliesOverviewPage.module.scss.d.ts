0b0f04939220096b59aef02ea898c40f19a696b3
jyotiarora-harness <jyoti.arora@harness.io>
2022-02-03 11:23:27 +0530

    feat: CCM-5383: Merging master

--- src/modules/75-ce/pages/anomalies-overview/AnomaliesOverviewPage.module.scss.d.ts
+++ src/modules/75-ce/pages/anomalies-overview/AnomaliesOverviewPage.module.scss.d.ts
@@ -1,11 +1,17 @@
 /* eslint-disable */
-// this is an auto-generated file
+/**
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ **/
+// this is an auto-generated file, do not update this manually
 declare const styles: {
-  readonly searchInput: string
-  readonly header: string
-  readonly groupbyFilter: string
-  readonly summaryCharts: string
   readonly cols: string
+  readonly groupbyFilter: string
+  readonly header: string
+  readonly searchInput: string
+  readonly summaryCharts: string
   readonly tableView: string
 }
 export default styles
