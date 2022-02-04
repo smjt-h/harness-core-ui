11df859d915a899b8b6a3490e46b40b3b1189ca9
Vardan Bansal <vardan.bansal@harness.io>
2022-02-04 09:52:21 +0530

    feat: [CI-3420]: Adding Pwsh shell type for AWS VM Infra (#7174)

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -474,6 +474,7 @@ export interface StringsMap {
   'common.purpose.setup': string
   'common.purpose.startATrial': string
   'common.purpose.welcome': string
+  'common.pwsh': string
   'common.readOnly': string
   'common.readonlyPermissions': string
   'common.recommended': string
