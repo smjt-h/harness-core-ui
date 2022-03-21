c029eca1ef5791e8d2043e026986baba7381edd1
mtran7 <michael.tran@harness.io>
2022-03-21 15:42:49 -0700

    fix: [CI-3867]: NPE on no tests hotfix (#7922)

--- src/modules/70-pipeline/pages/execution/ExecutionTestView/BuildTests.tsx
+++ src/modules/70-pipeline/pages/execution/ExecutionTestView/BuildTests.tsx
@@ -323,6 +323,10 @@ function BuildTests({ reportSummaryMock, testOverviewMock }: BuildTestsProps): R
   useEffect(() => {
     if (reportInfoData && testInfoData) {
       const uniqItems = uniqWith([...reportInfoData, ...testInfoData], isEqual)
+      if (uniqItems?.length < 1) {
+        return // no test results
+      }
+
       let uniqueStageIdOptions: SelectOption[] | any = [] // any includes additionally index for ordering below
       const uniqueStepIdOptionsFromStageKeyMap: { [key: string]: SelectOption[] | any } = {}
       const pipelineOrderedStagesMap: { [key: string]: number } = {}
@@ -343,6 +347,9 @@ function BuildTests({ reportSummaryMock, testOverviewMock }: BuildTestsProps): R
             label: `Step: ${step}`,
             value: step
           })
+          if (!uniqueStepIdOptionsFromStageKeyMap[stage].includes(AllStepsOption)) {
+            uniqueStepIdOptionsFromStageKeyMap[stage].unshift(AllStepsOption)
+          }
         } else if (stage && step) {
           uniqueStepIdOptionsFromStageKeyMap[stage] = [
             {
@@ -370,7 +377,6 @@ function BuildTests({ reportSummaryMock, testOverviewMock }: BuildTestsProps): R
         uniqueStepIdOptionsFromStageKeyMap[uniqueStageIdOptions[selectedStageIndex].value as string]
 
       if (selectedStepOptions.length > 1) {
-        selectedStepOptions.unshift(AllStepsOption)
         setSelectedStepId(selectedStepOptions[1])
       } else {
         setSelectedStepId(selectedStepOptions[0])
