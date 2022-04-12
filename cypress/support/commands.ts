733702cf8e249046f974f57ab874de705daf1d62
ishantmehtaharness <ishant@harness.io>
2022-04-12 18:50:26 +0530

    feat: [PIE-3444]: Add config for cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -80,16 +80,28 @@ declare global {
       verifyStepSelectConnector(): void
       verifyStepChooseRuntimeInput(): void
       verifyStepSelectStrategyAndVerifyStep(): void
-      matchImageSnapshot(name?: string): void
+      // https://github.com/jaredpalmer/cypress-image-snapshot
+      matchImageSnapshot(snapshotName?: string, options?: any): void
     }
   }
 }
 
+// We set up the settings
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.000001, // threshold for entire image
+  customSnapshotsDir: 'cypress/snapshots',
+  failureThreshold: 0.05, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
   customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
   capture: 'viewport' // capture viewport in screenshot
+})
+
+// We also overwrite the command, so it does not take a screenshot if we run the tests inside the test runner
+Cypress.Commands.overwrite('matchImageSnapshot', (originalFn, snapshotName, options) => {
+  if (Cypress.env('ALLOW_SCREENSHOT')) {
+    originalFn(snapshotName, options)
+  } else {
+    cy.log(`Screenshot comparison is disabled`)
+  }
 })
 
 Cypress.Commands.add('clickSubmit', () => {
