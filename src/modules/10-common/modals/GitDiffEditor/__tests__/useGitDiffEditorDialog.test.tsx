3daf3236861e3e974fc13601ae529e543d4a0b47
sunnykesh-harness <sunnykesh.shandilya@harness.io>
2022-04-13 15:48:19 +0530

    feat: [PL-24225]: Adding test for gitDiffEditor

--- src/modules/10-common/modals/GitDiffEditor/__tests__/useGitDiffEditorDialog.test.tsx
+++ src/modules/10-common/modals/GitDiffEditor/__tests__/useGitDiffEditorDialog.test.tsx
@@ -11,9 +11,77 @@ import { waitFor } from '@testing-library/dom'
 import { TestWrapper } from '@common/utils/testUtils'
 import { useGitDiffEditorDialog } from '../useGitDiffEditorDialog'
 
+const fetchRemoteFileContent = jest.fn()
 jest.mock('services/cd-ng', () => ({
-  useGetFileContent: jest.fn().mockImplementation(() => ({ refetch: jest.fn() }))
+  useGetFileContent: jest.fn().mockImplementation(() => ({ refetch: fetchRemoteFileContent }))
 }))
+
+const mockEntity = {
+  pipeline: {
+    stages: [
+      {
+        stage: {
+          type: 'Deployment',
+          spec: {
+            serviceConfig: { serviceRef: 'service_2', serviceDefinition: { type: 'Kubernetes', spec: {} } },
+            infrastructure: {
+              infrastructureDefinition: {
+                type: 'KubernetesDirect',
+                spec: { releaseName: 'release-<+INFRA_KEY>', namespace: '<+input>', connectorRef: '<+input>' }
+              },
+              environmentRef: 'dev2',
+              allowSimultaneousDeployments: false
+            },
+            execution: {
+              steps: [
+                {
+                  step: {
+                    type: 'K8sRollingDeploy',
+                    timeout: '10m',
+                    spec: { skipDryRun: false },
+                    name: 'Rollout Deployment',
+                    identifier: 'rolloutDeployment'
+                  }
+                }
+              ],
+              rollbackSteps: [
+                {
+                  step: {
+                    type: 'K8sRollingRollback',
+                    timeout: '10m',
+                    name: 'Rollback Rollout Deployment',
+                    identifier: 'rollbackRolloutDeployment',
+                    spec: {}
+                  }
+                }
+              ]
+            }
+          },
+          name: 'stage 2',
+          identifier: 'stage_2',
+          failureStrategies: [{ onFailure: { errors: ['AllErrors'], action: { type: 'StageRollback' } } }]
+        }
+      }
+    ],
+    projectIdentifier: 'dev',
+    orgIdentifier: 'dev_test',
+    name: 'pipeline 24',
+    identifier: 'pipeline_2',
+    allowStageExecutions: false
+  }
+}
+
+const mockGitDetails = {
+  repoIdentifier: 'repo',
+  rootFolder: '/src/.harness/',
+  filePath: 'pipeline_2.yaml',
+  commitMsg: 'Update pipeline pipeline 24',
+  createPr: false,
+  targetBranch: '',
+  isNewBranch: false,
+  branch: 'main',
+  resolvedConflictCommitId: ''
+}
 
 describe('Test hook for correctness', () => {
   test('render useGitDiffEditorDialog hook', async () => {
@@ -31,7 +99,8 @@ describe('Test hook for correctness', () => {
     expect(Object.keys(result.current).indexOf('hideGitDiffDialog')).not.toBe(-1)
     expect(Object.keys(result.current).indexOf('openGitDiffDialog')).not.toBe(-1)
     await waitFor(() => {
-      expect(result.current.openGitDiffDialog({})).toBe(undefined)
+      expect(result.current.openGitDiffDialog(mockEntity, mockGitDetails)).toBe(undefined)
+      expect(fetchRemoteFileContent).toBeCalledTimes(1)
     })
   })
 })
