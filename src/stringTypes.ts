5911bfc1c213e0f63fa9298aa4764ce9d934edf3
Tamaraius <tamara.galovic@harness.io>
2022-04-15 11:20:00 +0200

    fix: pr comments, delegateType name

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -582,7 +582,6 @@ export interface StringsMap {
   'common.searchOther': string
   'common.searchPlaceholder': string
   'common.seconds': string
-  'common.secret': string
   'common.secretKey': string
   'common.secrets': string
   'common.seeDetails': string
