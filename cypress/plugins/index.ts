3c376e0d46030996075dd9415904e4c677de0abf
ishantmehtaharness <ishant@harness.io>
2022-04-20 17:58:23 +0530

    fix trying snapshots

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -61,11 +61,11 @@ module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions)
 
   on('before:browser:launch', (browser, launchOptions) => {
     if (browser.name === 'chrome' && browser.isHeadless) {
-      // fullPage screenshot size is 1400x1200 on non-retina screens
+      // fullPage screenshot size is 2800x2800 on non-retina screens
       // and 2800x2400 on retina screens
-      launchOptions.args.push('--window-size=1400,1200')
+      launchOptions.args.push('--window-size=2800,2800')
 
-      // force screen to be non-retina (1400x1200 size)
+      // force screen to be non-retina (2800x2800 size)
       launchOptions.args.push('--force-device-scale-factor=1')
 
       // force screen to be retina (2800x2400 size)
@@ -73,16 +73,16 @@ module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions)
     }
 
     if (browser.name === 'electron' && browser.isHeadless) {
-      // fullPage screenshot size is 1400x1200
-      launchOptions.preferences.width = 1400
-      launchOptions.preferences.height = 1200
+      // fullPage screenshot size is 2800x2800
+      launchOptions.preferences.width = 2800
+      launchOptions.preferences.height = 2800
     }
 
     if (browser.name === 'firefox' && browser.isHeadless) {
       // menubars take up height on the screen
-      // so fullPage screenshot size is 1400x1126
-      launchOptions.args.push('--width=1400')
-      launchOptions.args.push('--height=1200')
+      // so fullPage screenshot size is 2800x1126
+      launchOptions.args.push('--width=2800')
+      launchOptions.args.push('--height=2800')
     }
 
     return launchOptions
